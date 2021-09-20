1ada33463970c6a86d523e0dcab4506c47331474
LouisTrouilleux <58275850+LouisTrouilleux@users.noreply.github.com>
2021-09-20 10:10:37 +0200

    Add files via upload

---
+++ cave-vin/pages/accueil.php
@@ -0,0 +1,79 @@
+
+
+<?= $diaporama_accueil ?>
+            <main class="main-wrapper">
+                <section class="art-wrapper">
+                    <div class="art-container">
+                        <article class="art art--accueil">
+                            <div class="art__col art__col-l">
+                                <h3 class="art-header__title">Cave à Vins, Bières, Spiritueux à Guipavas..</h3>
+                                <div class="art__txt">
+                                    <p><strong class="accroche">La Cave du Coin est un caviste indépendant situé à Guipavas, près de Brest dans le Finistère.</strong></p>
+                                    <p>Une large offre de Vins, Champagne, Spiritueux et Epicerie Fine : Vous recherchez un vin de qualité provenant
+                                    <a class="art__link" href="index?page=nos-vins"> d'un petit producteur ou d'un domaine plus réputé</a> 
+                                    , en culture bio, biodynamique ou encore naturelle ? Des références de<a class="art__link" href="index?page=whiskys-spiritueux"> Whiskys, Rhums ou Cognacs </a>
+                                    soigneusement sélectionnées pour leur qualité ? Ou encore des
+                                    <a class="art__link" href="index?page=nos-bieres"> Bières</a>,<a class="art__link" href="index?page=thes-cafes"> Thés</a>,<a class="art__link" href="index?page=thes-cafes"> Cafés</a>
+                                    et produits d’Epicerie Fine originaires de France et des quatre coins du Monde ? 
+                                    Vous trouverez votre bonheur grâce au grand choix disponible dans notre cave. 
+                                    Aux côtés des incontournables, des délices plus confidentiels ne demandent qu’à vous séduire…</p>
+                                    <a class="art__end-link" href="index?page=nos-vins">Nos Vins</a>
+                                </div>
+                            </div>
+                            <div class="art__col art__col-r">
+                                <img class="art__img" src="images/img1.jpg" alt="image article 1">
+                            </div>
+                        </article>
+                        <article class="art art--accueil">
+                            <div class="art__col art__col-l">
+                                <img class="art__img" src="images/img2.jpg" alt="imgage article 2">
+                            </div>
+                            <div class="art__col art__col-r">
+                                <h3 class="art-header__title">Les conseils de Cavistes passionnés..</h3>
+                                <div class="art__txt">
+                                    <p><strong class="accroche">En choisissant un Caviste indépendant et local tel que La Cave du Coin à Guipavas</strong>, 
+                                    vous bénéficierez d’un accueil avec le sourire, et des conseils personnalisés de nos Cavistes-Sommeliers professionnels,
+                                    qui partageront leur passion avec vous, notamment à travers des
+                                    <a class="art__link" href="index?page=cours-oenologie" target="_blank"><span style="text-decoration: underline;" > Cours d’Oenologie</span></a>.
+                                    </p>
+                                    <p>
+                                    Que vous recherchiez le joyau qui donnera de l’éclat à votre apéritif ou le vin qui 
+                                    s’accordera parfaitement avec le menu de votre réception, nous saurons vous guider vers le bon choix. 
+                                    </p>
+                                    <a class="art__end-link"  href="index?page=entreprises">Nos Services</a>
+                                </div>
+                            </div>
+                        </article>
+                    </div>    
+                </section>
+                <section id="Encarts">
+                    
+                </section>
+                <section id="socialWall">
+
+                </section>
+
+                <section class="contact-wall">
+                    <div class="contact-float">
+                        <a class="contact-float__link" href="index?page=contact">
+                            <div class="contact-float__content">
+                                <h2>
+                                    Contactez nous
+                                </h2>
+                                <p>
+                                    La Cave du Coin : pour toute question,
+                                    <br>
+                                    n'hésitez pas à nous appeler au
+                                </p>
+                                <p class="contact-float__phone">
+                                    02 29 02 60 47
+                                </p>
+                                <span class="contact-float__btn">Contactez nous</span>
+                            </div>
+                        </a>
+                    </div>
+                    <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2647.1274787969096!2d-4.403410684340558!3d48.434893079248276!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x4816b0627834a1a5%3A0x1c1c4018b6604f31!2sLa%20cave%20du%20coin!5e0!3m2!1sfr!2sfr!4v1631054279546!5m2!1sfr!2sfr" 
+                    width="100%" height="350" style="border:0;" allowfullscreen="" loading="lazy"></iframe>
+                </section>
+                
+            </main>
